ff6240417ca33d43ee57dbc37e169af9819a77ae
phvania <129224910+phvania@users.noreply.github.com>
2023-10-20 14:02:23 -0400

    filtered

--- client/src/pages/Sales.jsx
+++ client/src/pages/Sales.jsx
@@ -1,14 +1,25 @@
-
+import CategoryMenu from "../components/CategoryMenu";
+import ProductList from "../components/ProductList";
 const Sales = () => {
    return (
         <div>
             <h3> current Sales product</h3>
             <p>salesProductList</p> 
-            const { `Image, ProductItem price, quentity`} = ProductItem
+            {/* const { `Image, ProductItem price, quentity`} = ProductItem */}
+            <ProductList/>
         </div>
     )
 
-}           
+}   
+const filteredProdects = saleProducts
+   .filter((product)  => product.discountPercentage >= discountFilter)
+   .filter((product) => !CategoryMenu || product.CategoryFilter ===CategoryMenu);
+   
+   return (
+    <div>
+        
+    </div>
+   )
 
 
     
