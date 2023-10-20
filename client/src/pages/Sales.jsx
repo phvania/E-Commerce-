52ad44f35e553790f54c8fb4d900d14d1e5a11b2
phvania <129224910+phvania@users.noreply.github.com>
2023-10-20 12:50:25 -0400

    updated

--- client/src/pages/Sales.jsx
+++ client/src/pages/Sales.jsx
@@ -1,11 +1,23 @@
 
+import  { useState, useEffect} from 'react';
+ 
+import ProductItem from '../components/ProductItem';
+//import productCard from './productCard'
+import {getSaleProducts} from '../components/CategoryMenu'
+    
 const Sales = () => {
-    return (
+   return (
         <div>
-            <h3>Current Sales</h3>
+            <h3> current Sales product</h3>
+            <p>salesProductList</p> 
+            const { `Image, ProductItem price, quentity`} = ProductItem
         </div>
     )
 
 }
+
+
+    
+            
 
 export default Sales;
