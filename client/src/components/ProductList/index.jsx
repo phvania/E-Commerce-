a72a948f0e5003caff11e0dd53bbe32c437472f1
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-10-20 14:00:40 -0400

    temp

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -13,7 +13,7 @@ function ProductList() {
   const { currentCategory } = state;
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
+console.log(data)
   useEffect(() => {
     if (data) {
       dispatch({
