8e490974074fdc927b087ffa8cf6373bdb6c702b
phvania <129224910+phvania@users.noreply.github.com>
2023-10-21 18:51:13 -0400

    added filter

--- client/src/pages/Sales.jsx
+++ client/src/pages/Sales.jsx
@@ -1,5 +1,7 @@
 import CategoryMenu from "../components/CategoryMenu";
 import ProductList from "../components/ProductList";
+import filterCriteria from  "../components/models";
+import saleProducts from "../components/CategoryMenu"
 const Sales = () => {
    return (
         <div>
@@ -10,19 +12,57 @@ const Sales = () => {
         </div>
     )
 
-}   
-const filteredProdects = saleProducts
-   .filter((product)  => product.discountPercentage >= discountFilter)
-   .filter((product) => !CategoryMenu || product.CategoryFilter ===CategoryMenu);
-   
-   return (
-    <div>
-        
-    </div>
-   )
-
-
+   }
+   //filter data,based on the filer criteria /javascript'sarray method to ahieve this  
+const filteredProdects =  saleProducts.filter((product) => {
+return (
+    (filterCriteria.CategoryMenu === '' || product.CategoryMenu ===filterCriteria.CategoryMenu) &&
+    (filterCriteria.priceRange === 'all' ||
+    (filterCriteria.priceRange === 'low' && product.price <20) ||
+    (filterCriteria.priceRange === 'medium' && product.price >= 20 && product.price < 50) ||
+    (filterCriteria.priceRange === 'high' && product.price >= 50)
+)
+);
+});
     
-            
+ 
+   //.filter((product)  => product.discountPercentage >= discountFilter)]}
+   //.filter((product) => !CategoryMenu || product.CategoryFilter ===CategoryMenu);
+ 
+   
+    //const Sale = (
+        //render filter element
+        
+        const filteredProdects =  saleProducts.filter((product) => {
+       
+return (
+             <div className="sale-page">
+             <h2>on sale!</h2>
+            <div className="filter-section">
+            <label>Categorymanu:</label>
+            <input
+            type= "name"
+            value={discountFilter}
+           onChange={(e) => setDiscountFilter(e.target.value)} />
+            {/* <label>Category:</label>
+            <select
+            value={CategoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)} >
+                <option value="">All</option>
+                <option value= " Romantic Books">Romantic Books</option>
+                <option value= "Mystery Books">Mystery</option> */}
+            {/* </select> */}
+        </div>
+        <div className="product-list">
+            filteredProducts.map(product) =
+                CategoryMenu key={product.id} product={product}
+               </div>
 
+        </div>
+           );
+            };
+        )
+                          
+                    
+                      
 export default Sales;
